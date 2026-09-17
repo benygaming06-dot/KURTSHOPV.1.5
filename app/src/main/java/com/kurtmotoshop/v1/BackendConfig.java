ec4ae7bf992f18ec7682fc5da960dcac62d2df4c
benygaming06-dot <benygaming06@gmail.com>
2026-09-18 03:17:05 +0900

    Update BackendConfig.java

--- app/src/main/java/com/kurtmotoshop/v1/BackendConfig.java
+++ app/src/main/java/com/kurtmotoshop/v1/BackendConfig.java
@@ -2,9 +2,14 @@ package com.kurtmotoshop.v1;
 
 public final class BackendConfig {
 
-    public static final String FIREBASE_API_KEY = "ILAGAY_DITO_ANG_API_KEY";
-    public static final String FIREBASE_PROJECT_ID = "kurtshop-inventory";
-    public static final String SHOP_ID = "kurt-dhylan-moto-shop";
+    public static final String FIREBASE_API_KEY =
+        "AIzaSyCwwih0qe06ZE8JGRrQx9JN7bFl3MTg6-A";
+
+public static final String FIREBASE_PROJECT_ID =
+        "kurtshop-inventory";
+
+public static final String SHOP_ID =
+        "kurt-dhylan-moto-shop";
 
     private BackendConfig() {}
 }
